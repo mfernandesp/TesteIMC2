e57e8d06680d9d8968c570c05c3f1d33c9e0b886
mfernandesp <mfernandesporto1@gmail.com>
2018-08-25 14:39:57 -0300

    Initial commit

---
+++ app/src/main/java/com/marianeleite/testeimc/MyService.kt
@@ -0,0 +1,14 @@
+package com.marianeleite.testeimc
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+
+}
